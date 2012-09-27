dcfe3ed9dc6c1388b229a57635588c8aaac0301c
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-09-27 09:35:20 +0100

    message

--- tdd/src/main/java/com/ekanathk/tdd/algos/SumOfSubsets.java
+++ tdd/src/main/java/com/ekanathk/tdd/algos/SumOfSubsets.java
@@ -1,5 +1,10 @@
 package com.ekanathk.tdd.algos;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+
 /**
  * Created with IntelliJ IDEA.
  * User: kannan
@@ -8,4 +13,39 @@ package com.ekanathk.tdd.algos;
  * To change this template use File | Settings | File Templates.
  */
 public class SumOfSubsets {
+
+    private List<List<Boolean>> solutions = new ArrayList<List<Boolean>>();
+    private static final Logger logger = Logger.getLogger(SumOfSubsets.class.getName());
+
+    public static List<List<Boolean>> sumOfSubsets(List<Integer> inputs, Integer sum) {
+        SumOfSubsets s = new SumOfSubsets();
+        Collections.sort(inputs);
+        s.sumOfSubsets(inputs, fill(new ArrayList<Boolean>(), false, inputs.size()), 0, 0, sum);
+        return s.solutions;
+    }
+
+    private void sumOfSubsets(List<Integer> inputs, List<Boolean> subsetInclusion, int currentSum, int pointer, int totalSum) {
+        logger.info("Computing " + inputs + " weights " + subsetInclusion + " currentSum " + currentSum  + " pointer " + pointer + " totalSum " + totalSum);
+        if(pointer == inputs.size()) {
+            return;
+        }
+        subsetInclusion.set(pointer, true);
+        if(currentSum + inputs.get(pointer) == totalSum) {
+            logger.info("Found solution");
+            solutions.add(new ArrayList<Boolean>(subsetInclusion));
+            return;
+        }
+        if(currentSum + inputs.get(pointer) < totalSum) {
+            sumOfSubsets(inputs, subsetInclusion, currentSum + inputs.get(pointer), pointer + 1, totalSum);
+        }
+        subsetInclusion.set(pointer, false);
+        sumOfSubsets(inputs, subsetInclusion, currentSum, pointer + 1, totalSum);
+    }
+
+    private static <T> List<T> fill(List<T> list, T value, int count) {
+        for(int i = 0; i< count; i++) {
+            list.add(value);
+        }
+        return list;
+    }
 }
--- tdd/src/test/java/com/ekanathk/tdd/algos/SumOfSubsetTest.java
+++ tdd/src/test/java/com/ekanathk/tdd/algos/SumOfSubsetTest.java
@@ -1,5 +1,13 @@
 package com.ekanathk.tdd.algos;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.ekanathk.tdd.algos.SumOfSubsets.sumOfSubsets;
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created with IntelliJ IDEA.
  * User: kannan
@@ -8,4 +16,18 @@ package com.ekanathk.tdd.algos;
  * To change this template use File | Settings | File Templates.
  */
 public class SumOfSubsetTest {
+
+    @Test
+    public void testSimple() {
+        List<Integer>  inputs = Arrays.asList(-1, 2, 3, 5, 8, 9, 10);
+        List<List<Boolean>> sums = sumOfSubsets(inputs, 14);
+        for(List<Boolean> answer : sums) {
+            for(int i = 0; i < answer.size(); i++) {
+                if(answer.get(i)) {
+                    System.out.print(inputs.get(i) + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
