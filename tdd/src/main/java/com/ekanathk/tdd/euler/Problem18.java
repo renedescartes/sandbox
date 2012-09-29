d5b1b737eb0d255be1814ee5a6aaf49072c6f873
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-09-29 18:12:38 +0100

    stuff

--- tdd/src/main/java/com/ekanathk/tdd/euler/Problem18.java
+++ tdd/src/main/java/com/ekanathk/tdd/euler/Problem18.java
@@ -66,6 +66,6 @@ public class Problem18 {
                 {63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31},
                 {04, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 04, 23},
         };
-        assertEquals(23, maximalSum(array));
+        assertEquals(1074, maximalSum(array));
     }
 }
