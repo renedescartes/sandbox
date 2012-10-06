d0176663a82bb88c585cccd01e55eb133df1be18
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-06 09:35:32 +0100

    a

--- tdd/src/main/java/com/ekanathk/tdd/euler/Problem36.java
+++ tdd/src/main/java/com/ekanathk/tdd/euler/Problem36.java
@@ -2,7 +2,10 @@ package com.ekanathk.tdd.euler;
 
 import org.testng.annotations.Test;
 
+import java.util.logging.Logger;
+
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 /**
  * User: ekanathk
@@ -10,8 +13,11 @@ import static org.testng.Assert.assertEquals;
  */
 public class Problem36 {
 
-    private static String convertBinary(int number) {
-        int n = number;
+    private static final Logger logger = Logger.getLogger(Problem36.class.getName());
+
+    private static String convertBinary(long number) {
+        if(number == 0) { return "0";}
+        long n = number;
         StringBuffer b = new StringBuffer();
         while (n != 0) {
             b.append(n % 2);
@@ -31,9 +37,33 @@ public class Problem36 {
         return b.toString();
     }
 
+    private static boolean isBothBasePalindrome(Long n) {
+        return isPalindrome(n.toString()) && isPalindrome(convertBinary(n));
+    }
+
+    public static long sumOfPalindromeNumbers(Long n) {
+        long sum = 0;
+        for(long i = 1; i < n; i++) {
+            if(isBothBasePalindrome(i)) {
+                logger.info("Palindrome number " + i);
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
     @Test
     public void testSimple() {
         assertEquals("111", convertBinary(7));
         assertEquals("1001001001", convertBinary(585));
+        assertEquals("1", convertBinary(1));
+        assertEquals("0", convertBinary(0));
+        assertEquals("1010", convertBinary(10));
+        assertTrue(isBothBasePalindrome(585L));
+    }
+
+    @Test
+    public void testComplex() {
+        assertEquals(sumOfPalindromeNumbers(1000000L), 872187);
     }
 }
