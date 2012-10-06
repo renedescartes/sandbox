049bcc82f35d8c53aafe7a156abd0e7edc642c3a
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-06 18:24:15 +0100

    stuff

--- tdd/src/main/java/com/ekanathk/tdd/euler/Problem96.java
+++ tdd/src/main/java/com/ekanathk/tdd/euler/Problem96.java
@@ -4,6 +4,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.*;
+import java.util.logging.Logger;
 
 import static org.testng.Assert.assertEquals;
 
@@ -31,8 +32,31 @@ public class Problem96 {
         assertEquals(Sudoku.poolNumber(i, j), pool);
     }
 
+    @Test
+    public void testSolve() {
+        Sudoku s = new Sudoku(new int[][] {
+                {0,0,3,0,2,0,6,0,0},
+                {9,0,0,3,0,5,0,0,1},
+                {0,0,1,8,0,6,4,0,0},
+                {0,0,8,1,0,2,9,0,0},
+                {7,0,0,0,0,0,0,0,8},
+                {0,0,6,7,0,8,2,0,0},
+                {0,0,2,6,0,9,5,0,0},
+                {8,0,0,2,0,3,0,0,9},
+                {0,0,5,0,1,0,3,0,0},
+        });
+        Problem96 p = new Problem96();
+        s = p.solve(s);
+        System.out.println(s);
+    }
+
     private Sudoku answer = null;
+    private static final Logger logger = Logger.getLogger(Problem96.class.getName());
     public Sudoku solve(Sudoku input) {
+        logger.fine(input.toString());
+        if(answer != null) {
+            return answer;
+        }
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 if(input.getAt(i, j) == 0) {
@@ -42,6 +66,9 @@ public class Problem96 {
                     }
                 }
             }
+        }
+        if(input.isSolved()) {
+            answer = input;
         }
         return answer;
     }
@@ -68,13 +95,13 @@ class Sudoku {
         if(array[i][j] != 0) {throw new IllegalArgumentException("Attempting to findOptions an already solved piece");}
         Set<Integer> placements = new HashSet<>(options);
         //remove all elements in the first row
-        removeRowElements(array[i], options);
-        removeColumnElements(array, j, options);
-        removePoolElements(array, poolNumber(i, j), options);
+        removeRowElements(array[i], placements);
+        removeColumnElements(array, j, placements);
+        removePoolElements(array, poolNumber(i, j), placements);
         return placements;
     }
 
-    private void removePoolElements(int[][] array, int poolNumber, List<Integer> options) {
+    private void removePoolElements(int[][] array, int poolNumber, Collection<Integer> options) {
         for(int i = 0; i < array.length; i++) {
             for(int j = 0; j < array[i].length; j++) {
                 if(poolNumber == poolNumber(i, j)) {
@@ -94,8 +121,8 @@ class Sudoku {
     }
 
     private static void removeColumnElements(int[][] array, int columnNumber, Collection<Integer> options) {
-        for (int i = 0; i < array.length; i++) {
-            options.remove(array[i][columnNumber]);
+        for (int[] anArray : array) {
+            options.remove(anArray[columnNumber]);
         }
     }
 
@@ -110,7 +137,7 @@ class Sudoku {
 
     @Override
     public String toString() {
-        StringBuffer b = new StringBuffer();
+        StringBuffer b = new StringBuffer("\n");
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 b.append(array[i][j]).append(" ");
@@ -119,5 +146,16 @@ class Sudoku {
         }
         b.append("\n");
         return b.toString();
+    }
+
+    public boolean isSolved() {
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(array[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
