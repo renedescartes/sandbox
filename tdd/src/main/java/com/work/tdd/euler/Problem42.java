e7e7fd28c589303028502144e34165fed19be99d
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-09 12:40:53 +0100

    Mega refactor

--- tdd/src/main/java/com/work/tdd/euler/Problem42.java
+++ tdd/src/main/java/com/work/tdd/euler/Problem42.java
@@ -4,7 +4,6 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -14,18 +13,19 @@ public class Problem42 {
     private static List<Integer> scores(List<String> words) {
         List<Integer> scores = new ArrayList<>();
         for(String s : words) {
-            scores.add(score(s.substring(1, s.length()-1)));
+            String word = s.substring(1, s.length() - 1);
+            int score = score(word);
+            scores.add(score);
+            logger.fine("Word [" + word + "] score [" + score + "]");
         }
-        Collections.sort(scores);
         return scores;
     }
 
-    private static int countTriangular(List<Integer> numbers) {
-        logger.info(numbers.toString());
+    private static int countTriangular(List<Integer> numbers, List<String> words) {
         int count = 0;
-        for(Integer i : numbers) {
-            if(isTriangular(i)) {
-                logger.info(i.toString());
+        for(int i = 0;i < numbers.size(); i++) {
+            if(isTriangular(numbers.get(i))) {
+                logger.info("Triangular [" + words.get(i) + "] score [" + numbers.get(i) + "]");
                 count++;
             }
         }
@@ -33,7 +33,7 @@ public class Problem42 {
     }
 
     private static boolean isTriangular(int number) {
-        for(int i = 1; i < number; i++) {
+        for(int i = 1; i <= number; i++) {
             if((i * (i+1)/2) == number) {
                 return true;
             }
@@ -55,7 +55,8 @@ public class Problem42 {
     }
 
     public static int answer(String resource) {
-        return countTriangular(scores(readWords(resource)));
+        List<String> words = readWords(resource);
+        return countTriangular(scores(words), words);
     }
 
     @Test
