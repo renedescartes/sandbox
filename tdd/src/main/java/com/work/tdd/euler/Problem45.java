fc3ddf737e397d9bc68ad793461dd967489e7bf5
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-10 10:10:50 +0100

    Mega refactor

--- tdd/src/main/java/com/work/tdd/euler/Problem45.java
+++ tdd/src/main/java/com/work/tdd/euler/Problem45.java
@@ -2,56 +2,49 @@ package com.work.tdd.euler;
 
 import org.testng.annotations.Test;
 
+import java.util.logging.Logger;
+
+import static org.testng.Assert.*;
+
 public class Problem45 {
 
+    private static final Logger logger = Logger.getLogger(Problem45.class.getName());
+
     private static boolean isTriangular(long number) {
-        if(number == 1) {
-            return true;
-        }
-        for(int i = 1; i <= number; i++) {
-            if((i * (i+1)/2) == number) {
-                return true;
-            }
-        }
-        return false;
+        return isPerfectSquare((8 * number) + 1);
+    }
+
+    private static boolean isPerfectSquare(long number) {
+        double sqrt = Math.sqrt(number);
+        return sqrt * sqrt == number;
     }
 
     private static boolean isPentagonal(long number) {
-        if(number == 1) {
-            return true;
-        }
-        for(int i = 1; i <= number; i++) {
-            if((i * (3 * i -1)/2) == number) {
-                return true;
-            }
-        }
-        return false;
+        long check = (24 * number) + 1;
+        return isPerfectSquare(check) && (Math.sqrt(check) % 6 == 5);
     }
 
-    private static boolean isHexagonal(long number) {
-        if(number == 1) {
-            return true;
-        }
-        for(int i = 1; i <= number; i++) {
-            if((i * ( 2 * i - 1)) == number) {
-                return true;
+    public static long findCommon(Long from) {
+        long i = 1;
+        while (true) {
+            long p = (i * ( (2 * i) - 1));
+            logger.info("P = [" + p + "]");
+            if(p > from && isPentagonal(p) && isTriangular(p)) {
+                return p;
             }
+            i++;
         }
-        return false;
-    }
-
-    public static int findCommon(int from) {
-        while(!(isTriangular(from) && isPentagonal(from) && isHexagonal(from))) {
-            if(from % 10000 == 0) {
-                System.out.println(from);
-            }
-            from++;
-        }
-        return from;
     }
 
     @Test
-    public static void main(String[] args) {
-        System.out.println(findCommon(40756));
+    public void testSimple() {
+        assertTrue(isPerfectSquare(64));
+        assertTrue(isPerfectSquare(49));
+        assertTrue(isPerfectSquare(1));
+        assertFalse(isPerfectSquare(3));
+        assertFalse(isPerfectSquare(8));
+        assertTrue(isPentagonal(40755));
+        assertTrue(isTriangular(40755));
+        assertEquals(1533776805, findCommon(40755L));
     }
 }
