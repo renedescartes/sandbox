5d0d330229c4fa013b4e18d99a9dccfedce23f53
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-14 10:22:16 +0100

    stuff

---
+++ tdd/src/main/java/com/work/tdd/euler/card/TwoPairComputer.java
@@ -0,0 +1,22 @@
+package com.work.tdd.euler.card;
+
+import java.util.Collection;
+
+import static com.google.common.base.Predicates.not;
+import static com.google.common.collect.Collections2.filter;
+import static com.google.common.collect.Lists.reverse;
+import static com.work.tdd.euler.card.RankFunction.rankPredicate;
+
+public class TwoPairComputer implements RankComputer<ThreeKindRank> {
+    @Override
+    public ThreeKindRank handRank(Hand h) {
+        Rank rank = RankFunction.threeOfAKind(h);
+        Card[] otherCards = otherCards(h, rank);
+        return rank != null ? new ThreeKindRank(rank, otherCards[0].getRank(), otherCards[1].getRank()) : null;
+    }
+
+    private static Card[] otherCards(Hand h, Rank threeKindRank) {
+        Collection<Card> otherCards = filter(reverse(h.getCards()), not(rankPredicate(threeKindRank)));
+        return otherCards.toArray(new Card[otherCards.size()]);
+    }
+}
---
+++ tdd/src/main/java/com/work/tdd/euler/card/TwoPairRank.java
@@ -0,0 +1,32 @@
+package com.work.tdd.euler.card;
+
+import org.apache.commons.lang.builder.CompareToBuilder;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class TwoPairRank extends AbstractRank {
+
+    private Rank pair1Rank, pair2Rank, otherRank;
+
+    public TwoPairRank(Rank pair1Rank, Rank pair2Rank, Rank otherRank) {
+        checkNotNull(pair1Rank);
+        checkNotNull(pair2Rank);
+        checkNotNull(otherRank);
+        this.pair1Rank = pair1Rank;
+        this.pair2Rank = pair2Rank;
+        this.otherRank = otherRank;
+    }
+
+    @Override
+    public Integer getLevel() {
+        return 4;
+    }
+
+    @Override
+    int compareCurrentLevel(HandRank o) {
+        TwoPairRank next = (TwoPairRank) o;
+        return new CompareToBuilder().append(pair1Rank, next.pair1Rank).
+                append(pair2Rank, next.pair2Rank).
+                append(otherRank, next.otherRank).toComparison();
+    }
+}
