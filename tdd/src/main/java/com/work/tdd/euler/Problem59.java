5eb1cdf440888d5d374244b923fac423d63d4603
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-15 13:56:16 +0100

    Mega refactor

--- tdd/src/main/java/com/work/tdd/euler/Problem59.java
+++ tdd/src/main/java/com/work/tdd/euler/Problem59.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang.StringUtils;
 import org.testng.annotations.Test;
 
 import javax.annotation.Nullable;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.logging.Logger;
@@ -32,8 +33,29 @@ public class Problem59 {
         });
     }
 
+    private static int[] decryptMessage(int[] encryptedMessage) {
+        int[] xorBuffers = new int[encryptedMessage.length];
+        int xor = 0;
+        for(int i = 0 ; i < encryptedMessage.length; i++) {
+            xor = xor ^ encryptedMessage[i];
+            xorBuffers[i] = xor;
+        }
+        int[] decryptedMessages = new int[encryptedMessage.length];
+        for(int i = encryptedMessage.length - 1; i > 0; i--) {
+            decryptedMessages[i] = xorBuffers[i] ^ xorBuffers[i-1];
+        }
+        decryptedMessages[0] = xorBuffers[1] ^ decryptedMessages[1];
+        return decryptedMessages;
+    }
+
     @Test
     public void testSimple() {
-        sumOfASCIIMessage("cipher1.txt");
+        int[] encrypt = new int[] {74, 7, 13, 6};
+        System.out.println(Arrays.toString(encrypt));
+        System.out.println(Arrays.toString(new int[]{74, 74 ^ 7, 74 ^ 7 ^ 13, 74 ^ 7 ^ 13 ^ 6}));
+        System.out.println(Arrays.toString(new int[]{74, 77 ^ 74, 64 ^ 77, 70^ 64}));
+
+        System.out.println(Arrays.toString(decryptMessage(encrypt)));
+        //sumOfASCIIMessage("cipher1.txt");
     }
 }
