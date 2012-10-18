cc133754009d84530dd08cdfe84c02bbd5f1d8ed
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-18 16:59:25 +0100

    Mega refactor

--- tdd/src/main/java/com/work/tdd/json/Visualise.java
+++ tdd/src/main/java/com/work/tdd/json/Visualise.java
@@ -3,20 +3,17 @@ package com.work.tdd.json;
 import com.google.gson.Gson;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 class EventType {
-    private Long id;
-    private String eventType;
+    private Event eventType;
     private Long cardinality;
 
-    EventType(Long id, String eventType, Long cardinality) {
-        this.id = id;
-        this.eventType = eventType;
+    EventType(Event e, Long cardinality) {
+        this.eventType = e;
         this.cardinality = cardinality;
     }
 }
+
 
 class Event {
     private Long id;
@@ -32,15 +29,10 @@ public class Visualise {
 
     public static void main(String[] args) {
         Gson gson = new Gson();
-        EventType e1 = new EventType(55L, "Football", 25L);
-        EventType e2 = new EventType(56L, "Baseball", 45L);
+        EventType e1 = new EventType(new Event(55L, "Football"), 25L);
+        EventType e2 = new EventType(new Event(56L, "Baseball"), 25L);
         System.out.println(gson.toJson(Arrays.asList(e1, e2)));
 
-
-        Map<Event, Long> map = new HashMap<>();
-        map.put(new Event(55L, "Football"), 25L);
-        map.put(new Event(56L, "Baseball"), 45L);
-        System.out.println(gson.toJson(map));
 
     }
 }
