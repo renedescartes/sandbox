20e8c1c31eccb7a29b7751b980da5b7d4bd15c7c
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-19 22:10:49 +0100

    stuff

--- tdd/src/main/java/com/work/tdd/euler/Problem76.java
+++ tdd/src/main/java/com/work/tdd/euler/Problem76.java
@@ -1,9 +1,11 @@
 package com.work.tdd.euler;
 
-import org.apache.commons.lang.StringUtils;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.TreeMultimap;
 import org.testng.annotations.Test;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.logging.Logger;
 
 import static org.testng.Assert.assertEquals;
@@ -11,66 +13,52 @@ import static org.testng.Assert.assertEquals;
 public class Problem76 {
     private static final Logger logger = Logger.getLogger(Problem76.class.getName());
 
-    private static Map<Integer, Set<String>> splits = new HashMap<>();
-
-    static {
-        splits.put(1, asTreeSet("1"));
-        splits.put(2, asTreeSet("11"));
-        splits.put(3, asTreeSet("12"));
-        splits.put(4, asTreeSet("31", "22", "211", "1111"));
-    }
-
-    public static Set<String> cachedSplitSum(int n) {
-        if (!splits.containsKey(n)) {
-            splits.put(n, splitSum(n));
+    public static Multimap<Integer, String> splitSum(int n) {
+        int level = 2;
+        Multimap<Integer, String> map = TreeMultimap.create();
+        for (int i = 1; i < n; i++) {
+            map.put(level, sortString("" + i + (n - i)));
         }
-        return splits.get(n);
-    }
-
-    public static Set<String> splitSum(int n) {
-        Set<String> answers = new TreeSet<>();
-        for (int i = 1; i <= n / 2; i++) {
-            answers.add("" + i + (n - i));
-            Set<String> morePermutes1 = cachedSplitSum(i);
-            Set<String> morePermutes2 = cachedSplitSum(n - i);
-            for (String p1 : morePermutes1) {
-                for (String p2 : morePermutes2) {
-                    answers.add(sortString(p1 + p2));
+        while (level <= n) {
+            Collection<String> levelStrings = map.get(level);
+            level++;
+            for (String s : levelStrings) {
+                Integer[] digits = Utility.digits(s);
+                for (int i = 0; i < digits.length; i++) {
+                    if (digits[i] != 1) {
+                        for (int j = 1; j < digits[i]; j++) {
+                            String newAttempt = s.substring(0, i) + j + (digits[i] - j) + s.substring(i + 1);
+                            map.put(level, sortString(newAttempt));
+                        }
+                    }
                 }
             }
         }
-        return answers;
+        return map;
     }
 
     private static String sortString(String s) {
         Integer[] digits = Utility.digits(s);
         Arrays.sort(digits);
-        return StringUtils.join(digits);
-    }
-
-    @SafeVarargs
-    private static <T> Set<T> asTreeSet(T... element) {
-        TreeSet<T> set = new TreeSet<>();
-        Collections.addAll(set, element);
-        return set;
+        StringBuilder b = new StringBuilder();
+        for (int i = digits.length - 1; i > 0; i--) {
+            b.append(digits[i]).append(" ");
+        }
+        b.append(digits[0]);
+        return b.toString();
     }
 
     @Test
     public void testSimple() {
-        Set<String> strings = cachedSplitSum(100);
+        Multimap<Integer, String> strings = splitSum(100);
         logger.info(strings.toString());
         assertEquals(strings.size(), 100);
     }
 
     @Test
     public void testBits() {
-        Set<String> strings = cachedSplitSum(4);
+        Multimap<Integer, String> strings = splitSum(6);
         logger.info(strings.toString());
-        logger.info(cachedSplitSum(5).toString());
-        logger.info(cachedSplitSum(6).toString());
-        logger.info(cachedSplitSum(7).toString());
-        logger.info(cachedSplitSum(8).toString());
-        assertEquals(strings.size(), 4);
     }
 }
 
