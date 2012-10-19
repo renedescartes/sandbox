f29b7117479e439f6f691439dc5e4e718510c5dd
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-19 22:53:25 +0100

    stuff

--- tdd/src/main/java/com/work/tdd/euler/Problem76.java
+++ tdd/src/main/java/com/work/tdd/euler/Problem76.java
@@ -1,6 +1,7 @@
 package com.work.tdd.euler;
 
 import com.google.common.collect.Lists;
+import org.apache.commons.lang.builder.CompareToBuilder;
 import org.testng.annotations.Test;
 
 import java.util.*;
@@ -13,14 +14,14 @@ public class Problem76 {
 
     public static long splitSum(int n) {
         int level = 2;
-        Set<List<Integer>> permutes = new HashSet<>();
+        Set<List<Integer>> permutes = new TreeSet<>(listComparator());
         for (int i = 1; i <= n / 2; i++) {
             permutes.add(Arrays.asList(i, (n - i)));
         }
         long sum = permutes.size();
-        Set<List<Integer>> newPermutes = new HashSet<>();
+        Set<List<Integer>> newPermutes = new TreeSet<>(listComparator());
         while (level <= n) {
-            logger.info("Level [" + level + "] stepCount [" + permutes.size() + "] count [" + sum + "]");
+            logger.info("Level [" + level + "] stepCount [" + permutes.size() + "] count [" + sum + "] options " + permutes);
             for (List<Integer> digits : permutes) {
                 for (int i = 0; i < digits.size(); i++) {
                     if (digits.get(i) != 1) {
@@ -37,10 +38,23 @@ public class Problem76 {
             }
             permutes = newPermutes;
             sum += permutes.size();
-            newPermutes = new HashSet<>();
+            newPermutes = new TreeSet<>(listComparator());
             level++;
         }
         return sum;
+    }
+
+    private static Comparator<? super List<Integer>> listComparator() {
+        return new Comparator<List<Integer>>() {
+            @Override
+            public int compare(List<Integer> o1, List<Integer> o2) {
+                CompareToBuilder e = new CompareToBuilder();
+                for (int i = 0; i < o1.size(); i++) {
+                    e.append(o1.get(i), o2.get(i));
+                }
+                return e.toComparison();
+            }
+        };
     }
 
     @Test
@@ -50,7 +64,7 @@ public class Problem76 {
 
     @Test
     public void testBits() {
-        assertEquals(splitSum(10), 41);
+        assertEquals(splitSum(11), 41);
     }
 }
 
