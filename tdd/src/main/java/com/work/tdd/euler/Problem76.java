4fde7c0429f6ba71c2210268f8d62b5ba8ff7f3c
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-19 22:34:00 +0100

    stuff

--- tdd/src/main/java/com/work/tdd/euler/Problem76.java
+++ tdd/src/main/java/com/work/tdd/euler/Problem76.java
@@ -1,11 +1,9 @@
 package com.work.tdd.euler;
 
-import com.google.common.collect.Multimap;
-import com.google.common.collect.TreeMultimap;
+import com.google.common.collect.Lists;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.*;
 import java.util.logging.Logger;
 
 import static org.testng.Assert.assertEquals;
@@ -13,52 +11,46 @@ import static org.testng.Assert.assertEquals;
 public class Problem76 {
     private static final Logger logger = Logger.getLogger(Problem76.class.getName());
 
-    public static Multimap<Integer, String> splitSum(int n) {
+    public static long splitSum(int n) {
         int level = 2;
-        Multimap<Integer, String> map = TreeMultimap.create();
-        for (int i = 1; i < n; i++) {
-            map.put(level, sortString("" + i + (n - i)));
+        Set<List<Integer>> permutes = new HashSet<>();
+        for (int i = 1; i <= n / 2; i++) {
+            permutes.add(Arrays.asList(i, (n - i)));
         }
+        long sum = permutes.size();
+        Set<List<Integer>> newPermutes = new HashSet<>();
         while (level <= n) {
-            Collection<String> levelStrings = map.get(level);
-            level++;
-            for (String s : levelStrings) {
-                Integer[] digits = Utility.digits(s);
-                for (int i = 0; i < digits.length; i++) {
-                    if (digits[i] != 1) {
-                        for (int j = 1; j < digits[i]; j++) {
-                            String newAttempt = s.substring(0, i) + j + (digits[i] - j) + s.substring(i + 1);
-                            map.put(level, sortString(newAttempt));
+            logger.info("Level [" + level + "] count [" + sum + "] permutes [" + permutes + "]");
+            for (List<Integer> digits : permutes) {
+                for (int i = 0; i < digits.size(); i++) {
+                    if (digits.get(i) != 1) {
+                        for (int j = 1; j < digits.get(i); j++) {
+                            List<Integer> newAttempt = Lists.newArrayList(digits.subList(0, i));
+                            newAttempt.add(j);
+                            newAttempt.add(digits.get(i) - j);
+                            newAttempt.addAll(digits.subList(i + 1, digits.size()));
+                            Collections.sort(newAttempt);
+                            newPermutes.add(newAttempt);
                         }
                     }
                 }
             }
+            permutes = newPermutes;
+            sum += permutes.size();
+            newPermutes = new HashSet<>();
+            level++;
         }
-        return map;
-    }
-
-    private static String sortString(String s) {
-        Integer[] digits = Utility.digits(s);
-        Arrays.sort(digits);
-        StringBuilder b = new StringBuilder();
-        for (int i = digits.length - 1; i > 0; i--) {
-            b.append(digits[i]).append(" ");
-        }
-        b.append(digits[0]);
-        return b.toString();
+        return sum;
     }
 
     @Test
     public void testSimple() {
-        Multimap<Integer, String> strings = splitSum(100);
-        logger.info(strings.toString());
-        assertEquals(strings.size(), 100);
+
     }
 
     @Test
     public void testBits() {
-        Multimap<Integer, String> strings = splitSum(6);
-        logger.info(strings.toString());
+        assertEquals(splitSum(6), 10);
     }
 }
 
