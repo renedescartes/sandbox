6a49d18983608f55a371b64d7ef8af3ec03bbb62
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-21 20:45:56 +0100

    stuff

--- tdd/src/main/java/com/work/tdd/euler/Problem76New.java
+++ tdd/src/main/java/com/work/tdd/euler/Problem76New.java
@@ -1,46 +1,25 @@
 package com.work.tdd.euler;
 
-import org.testng.annotations.Test;
+import com.google.common.collect.DiscreteDomains;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Ranges;
+import com.work.tdd.euler.util.Combinations;
 
+import java.util.List;
 import java.util.logging.Logger;
-
-import static org.testng.Assert.assertEquals;
 
 public class Problem76New {
 
     private static final Logger logger = Logger.getLogger(Problem76New.class.getName());
 
-    public static long explore(final int start, final int currentTotal, final int currentItems, final int requiredItems, final int requiredTotal) {
-        logger.info("Start " + start + " currentTotal " + currentTotal + " currentItems " + currentItems);
-        if (currentTotal == requiredTotal && currentItems == requiredItems) {
-            return 1;
+    public static long f(int n, int blocks) {
+        int slots = blocks - 1;
+        if (slots == 1) {
+            return n / 2;
         }
-        if (currentTotal >= requiredTotal || currentItems == requiredItems) {
-            return 0;
-        }
-        long sum = 0;
-        for (int i = start; i <= requiredTotal / requiredItems; i++) {
-            for (int j = 0; j <= requiredItems - currentItems; j++) {
-                int newCurrentTotal = currentTotal + (j * i);
-                if (newCurrentTotal > requiredTotal) {
-                    break;
-                }
-                if (i + 1 > requiredTotal / requiredItems) {
-                    break;
-                }
-                sum += explore(i + 1, newCurrentTotal, currentItems + j, requiredItems, requiredTotal);
-            }
-        }
-        return sum;
-    }
-
-    public static long explore(final int requiredItems, final int requiredTotal) {
-        return explore(1, 0, 0, requiredItems, requiredTotal);
-    }
-
-    @Test
-    public void testSimple() {
-        assertEquals(explore(6, 12), 11);
+        List<Integer> elements = Lists.newArrayList(Ranges.closed(1, n).asSet(DiscreteDomains.integers()));
+        Iterable<List<Integer>> permutes = Combinations.combinationIterator(elements, slots);
+        return 1;
     }
 }
 
