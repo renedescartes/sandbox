d52e34306b2ff60de7ee882b70388bc395908f02
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-22 08:12:11 +0100

    stuff

--- tdd/src/main/java/com/work/tdd/euler/Problem76New.java
+++ tdd/src/main/java/com/work/tdd/euler/Problem76New.java
@@ -2,25 +2,28 @@ package com.work.tdd.euler;
 
 import org.testng.annotations.Test;
 
-import java.util.logging.Logger;
-
 import static org.testng.Assert.assertEquals;
 
 public class Problem76New {
 
-    private static final Logger logger = Logger.getLogger(Problem76New.class.getName());
+    private static long[][] array = new long[1000][1000];
 
-    public static long perm(long k, long n) {
+    public static long perm(int k, int n) {
+        if (array[k][n] != 0) {
+            return array[k][n];
+        }
         if (k > n) {
             return 0;
         }
         if (k == n) {
             return 1;
         }
-        return perm(k, n - k) + perm(k + 1, n);
+        long perm = perm(k, n - k) + perm(k + 1, n);
+        array[k][n] = perm;
+        return perm;
     }
 
-    public static long answer(long n) {
+    public static long answer(int n) {
         return perm(1, n) - 1;
     }
 
@@ -32,7 +35,7 @@ public class Problem76New {
         assertEquals(answer(8), 21);
         assertEquals(answer(10), 41);
         assertEquals(answer(12), 76);
-        assertEquals(answer(100), 76);
+        assertEquals(answer(100), 190569291);
     }
 }
 
