05440eb77250fa3047a433e5daab18efda25acfe
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-24 10:43:11 +0100

    Mega refactor

--- tdd/src/main/java/com/work/tdd/euler/Problem64.java
+++ tdd/src/main/java/com/work/tdd/euler/Problem64.java
@@ -1,6 +1,30 @@
 package com.work.tdd.euler;
 
-public class Problem64 {
+import com.work.tdd.euler.util.fraction.RootFraction;
+import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class Problem64 {
+    private static final Logger logger = Logger.getLogger(Problem64.class.getName());
+
+    public List<Integer> rootFunction(int n) {
+        List<Integer> roots = new ArrayList<>();
+        RootFraction f = RootFraction.forSquareRoot(n);
+        while(!f.isSolved()) {
+            logger.info("Root fraction " + f);
+            int closest = f.closest();
+            roots.add(closest);
+            f = f.minus(closest).reciprocal().normalise();
+        }
+        return roots;
+    }
+
+    @Test
+    public void testSimple() {
+        System.out.println(rootFunction(23));
+    }
 
 }
--- tdd/src/main/java/com/work/tdd/euler/util/fraction/RootFraction.java
+++ tdd/src/main/java/com/work/tdd/euler/util/fraction/RootFraction.java
@@ -1,5 +1,7 @@
 package com.work.tdd.euler.util.fraction;
 
+import com.google.common.base.Preconditions;
+
 class Term {
     int realTerm;
     int rootTerm;
@@ -17,16 +19,66 @@ class Term {
         return new Term(rootTerm, -1 * realTerm);
     }
 
-    int closest() {
-        return (int) (Math.sqrt(rootTerm) + realTerm);
+    double closest() {
+        return (Math.sqrt(rootTerm) + realTerm);
+    }
+
+    boolean isRoot() {
+        return rootTerm != 0;
+    }
+
+    boolean isOne() {
+        return !isRoot() && realTerm == 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Root(" + rootTerm + ") + (" + realTerm + ")";
     }
 }
 
 public class RootFraction {
     private Term num, den;
 
-    public RootFraction(Term num, Term den) {
+    RootFraction(Term num, Term den) {
         this.num = num;
         this.den = den;
+    }
+
+    public RootFraction normalise() {
+        Preconditions.checkState(den.isRoot(), "Denominator should be root");
+        Preconditions.checkState(!num.isRoot(), "Numerator should not be root");
+        int f = (den.rootTerm * den.rootTerm - den.realTerm * den.realTerm) / num.realTerm;
+        return new RootFraction(den.converse(), new Term(f));
+    }
+
+    public RootFraction minus(int n) {
+        Preconditions.checkState(!den.isRoot(), "Denominator should not be root");
+        int newRealTerm = num.realTerm - (n * den.realTerm);
+        return new RootFraction(new Term(newRealTerm, num.rootTerm), den);
+    }
+
+    public int closest() {
+        return (int) ((num.closest()/den.closest()));
+    }
+
+    public static RootFraction forSquareRoot(int n) {
+        return new RootFraction(new Term(0, n), new Term(1));
+    }
+
+    public boolean isSolved() {
+        return den.isOne();
+    }
+
+    public RootFraction reciprocal() {
+        return new RootFraction(den, num);
+    }
+
+    @Override
+    public String toString() {
+        return "RootFraction{" +
+                "num=" + num +
+                ", den=" + den +
+                '}';
     }
 }
