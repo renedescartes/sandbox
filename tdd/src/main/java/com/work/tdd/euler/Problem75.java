3fa32bb1b2cb72b8e4ebdcc9a800ec924047b4dd
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-24 18:13:33 +0100

    Mega refactor

--- tdd/src/main/java/com/work/tdd/euler/Problem75.java
+++ tdd/src/main/java/com/work/tdd/euler/Problem75.java
@@ -2,10 +2,10 @@ package com.work.tdd.euler;
 
 import org.apache.commons.lang.builder.CompareToBuilder;
 import org.testng.annotations.Test;
+import org.testng.internal.annotations.Sets;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.TreeSet;
 import java.util.logging.Logger;
 
 class Triple implements Comparable<Triple> {
@@ -26,38 +26,61 @@ class Triple implements Comparable<Triple> {
 
     @Override
     public String toString() {
-        return a + ", " + b + ", " + c;
+        return "{" + a + ", " + b + ", " + c + "}";
+    }
+
+    long perimeter() {
+        return a + b + c;
+    }
+
+    Triple times(int factor) {
+        return new Triple(a * factor, b * factor, c * factor);
     }
 }
 
 public class Problem75 {
     private static final Logger logger = Logger.getLogger(Problem75.class.getName());
 
-    public static Collection<Triple> explorePrimitiveTriples(long MAX) {
-        Collection<Triple> triples = new TreeSet<>();
+    public static Object[] explorePrimitiveTriples(int MAX) {
+        Object[] array = new Object[MAX];
         for (long n = 1; n < MAX; n++) {
             for (long m = n + 1; m < MAX; m++) {
                 if (Utility.gcd(m, n) == 1 && ((m - n) % 2 == 1)) {
                     long a = m * m - n * n;
                     long b = 2 * m * n;
                     long c = m * m + n * n;
-                    if (a + b + c > MAX) {
+                    if ((a + b + c) > (long) MAX) {
                         break;
                     }
-                    triples.add(new Triple(a, b, c));
-                    logger.fine("Primitive (" + a + "," + b + "," + c + ")");
+                    Triple triple = new Triple(a, b, c);
+                    Triple original = triple;
+                    int factor = 1;
+                    long perimeter;
+                    while ((perimeter = triple.perimeter()) < MAX) {
+                        if (array[(int) perimeter] == null) {
+                            array[(int) perimeter] = Sets.<Triple>newHashSet();
+                        }
+                        @SuppressWarnings("unchecked")
+                        Collection<Triple> triples = (Collection<Triple>) array[(int) perimeter];
+                        triples.add(triple);
+                        logger.fine("Triple [" + triple + "]");
+                        factor++;
+                        triple = original.times(factor);
+                    }
                 }
             }
         }
-        return triples;
+        return array;
     }
 
     @Test
     public void testBits() {
-        Collection<Triple> triples = explorePrimitiveTriples(300);
-        for (Triple triple : triples) {
-            logger.info("Triple [" + triple + "]");
+        Object[] triplesCollection = explorePrimitiveTriples(1500000);
+        for (int i = 0; i < triplesCollection.length; i++) {
+            Object triple = triplesCollection[i];
+            if (triple != null) {
+                logger.info("i = [" + i + "] triples " + triple);
+            }
         }
-
     }
 }
