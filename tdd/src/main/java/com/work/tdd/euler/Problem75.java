603123b30eb2269f717d6d7210e1cf86d84a5ac2
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-24 17:04:08 +0100

    Mega refactor

--- tdd/src/main/java/com/work/tdd/euler/Problem75.java
+++ tdd/src/main/java/com/work/tdd/euler/Problem75.java
@@ -1,13 +1,40 @@
 package com.work.tdd.euler;
 
+import org.apache.commons.lang.builder.CompareToBuilder;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.TreeSet;
 import java.util.logging.Logger;
+
+class Triple implements Comparable<Triple> {
+    long a, b, c;
+
+    Triple(long a, long b, long c) {
+        long[] array = new long[]{a, b, c};
+        Arrays.sort(array);
+        this.a = array[0];
+        this.b = array[1];
+        this.c = array[2];
+    }
+
+    @Override
+    public int compareTo(Triple o) {
+        return new CompareToBuilder().append(a, o.a).append(b, o.b).append(c, o.c).toComparison();
+    }
+
+    @Override
+    public String toString() {
+        return a + ", " + b + ", " + c;
+    }
+}
 
 public class Problem75 {
     private static final Logger logger = Logger.getLogger(Problem75.class.getName());
 
-    public static void explorePrimitiveTriples(long MAX) {
+    public static Collection<Triple> explorePrimitiveTriples(long MAX) {
+        Collection<Triple> triples = new TreeSet<>();
         for (long n = 1; n < MAX; n++) {
             for (long m = n + 1; m < MAX; m++) {
                 if (Utility.gcd(m, n) == 1 && ((m - n) % 2 == 1)) {
@@ -17,14 +44,20 @@ public class Problem75 {
                     if (a + b + c > MAX) {
                         break;
                     }
-                    logger.info("Primitive (" + a + "," + b + "," + c + ")");
+                    triples.add(new Triple(a, b, c));
+                    logger.fine("Primitive (" + a + "," + b + "," + c + ")");
                 }
             }
         }
+        return triples;
     }
 
     @Test
     public void testBits() {
-        explorePrimitiveTriples(300);
+        Collection<Triple> triples = explorePrimitiveTriples(300);
+        for (Triple triple : triples) {
+            logger.info("Triple [" + triple + "]");
+        }
+
     }
 }
