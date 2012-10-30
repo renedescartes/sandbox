cad062a89e2f109ff5614b4155afb2a32cd966bd
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-10-30 14:54:02 +0000

    Mega refactor

---
+++ tdd/src/main/java/com/work/tdd/euler/hard/Problem86.java
@@ -0,0 +1,58 @@
+package com.work.tdd.euler.hard;
+
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import static com.work.tdd.euler.util.NumberUtil.isPerfectSquare;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertFalse;
+
+public class Problem86 {
+    private static final Logger logger = Logger.getLogger(Problem86.class.getName());
+
+    private static Map<Long, Long> cache = new HashMap<>();
+
+    private boolean isIntegralCuboidShortestDistance(long l, long b, long w) {
+        return isPerfectSquare(Math.pow(l + b, 2) + Math.pow(w, 2)) ||
+                isPerfectSquare(Math.pow(w + b, 2) + Math.pow(l, 2)) ||
+                isPerfectSquare(Math.pow(l + w, 2) + Math.pow(b, 2));
+    }
+
+    public long cachedNumberOfCuboids(long n) {
+        if (!cache.containsKey(n)) {
+            cache.put(n, numberOfCuboids(n));
+        }
+        return cache.get(n);
+    }
+
+    private Long numberOfCuboids(long n) {
+        if (n == 1) {
+            return 0L;
+        }
+        long sum = 0;
+        for (long length = 1; length <= n; length++) {
+            for (long breadth = 1; breadth <= length; breadth++) {
+                for (long height = 1; height <= breadth; height++) {
+                    if (isIntegralCuboidShortestDistance(length, breadth, height)) {
+                        System.out.println(length + ", " + breadth + ", " + height);
+                        sum++;
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+
+    @Test
+    public void testBits() {
+        assertTrue(isIntegralCuboidShortestDistance(6, 5, 3));
+        assertFalse(isIntegralCuboidShortestDistance(7, 5, 3));
+        assertFalse(isIntegralCuboidShortestDistance(3, 3, 2));
+        assertEquals(cachedNumberOfCuboids(99), 1975);
+
+    }
+}
