d2ed96f6620ca14ba480154311ed82bf9e249249
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-11-01 17:44:59 +0000

    Mega refactor

--- tdd/src/main/java/com/work/tdd/euler/hard/Problem97.java
+++ tdd/src/main/java/com/work/tdd/euler/hard/Problem97.java
@@ -16,6 +16,19 @@ public class Problem97 {
         return n.length() <= length ? n : n.substring(n.length() - length, n.length());
     }
 
+    public static BigInteger findLastDigits(int a, int power, int length) {
+        BigInteger powerOfTwo = BigInteger.valueOf(2);
+        for (int i = 2; i <= power; i++) {
+            powerOfTwo = trimmed(powerOfTwo.multiply(BigInteger.valueOf(2)), length);
+        }
+        return trimmed(powerOfTwo.multiply(BigInteger.valueOf(a)).add(BigInteger.ONE), length);
+    }
+
+    @Test
+    public void testSimple() {
+        assertEquals(findLastDigits(28433, 7830457, 10).toString(), "8739992577");
+    }
+
     @Test
     public void testParts() {
         assertEquals(trimmed(new BigInteger("925"), 4).intValue(), 925);
