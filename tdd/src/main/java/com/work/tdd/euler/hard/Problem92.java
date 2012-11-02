d250f4dff958c9bab02fd20dd0349bdc56d18188
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-11-02 13:19:48 +0000

    Mega refactor

--- tdd/src/main/java/com/work/tdd/euler/hard/Problem92.java
+++ tdd/src/main/java/com/work/tdd/euler/hard/Problem92.java
@@ -1,4 +1,58 @@
 package com.work.tdd.euler.hard;
 
+import com.work.tdd.euler.medium.Utility;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import static org.testng.Assert.assertEquals;
+
 public class Problem92 {
+    private static final Logger logger = Logger.getLogger(Problem94.class.getName());
+
+    public static long answer(int target, int MAX) {
+        int[] array = new int[MAX + 1];
+        array[0] = -1;
+        array[1] = 1;
+        array[89] = 89;
+        long answer = 0;
+        int count = 2;
+        while (count < MAX) {
+            int sum = count;
+            List<Integer> path = new ArrayList<>();
+            while (array[sum] == 0) {
+                path.add(sum);
+                sum = sumOfSquareOfDigits(sum);
+            }
+            for (Integer pathEntry : path) {
+                array[pathEntry] = array[sum];
+            }
+            if (array[sum] == target) {
+                answer++;
+            }
+            count++;
+        }
+        return answer;
+    }
+
+    private static int sumOfSquareOfDigits(int n) {
+        Integer[] digits = Utility.digits((long) n);
+        int sum = 0;
+        for (Integer digit : digits) {
+            sum += (digit * digit);
+        }
+        return sum;
+    }
+
+    @Test
+    public void testSimple() {
+        assertEquals(answer(89, 10_000_000), 8581146);
+    }
+
+    @Test
+    public void testBits() {
+        assertEquals(answer(89, 1000), 857);
+    }
 }
