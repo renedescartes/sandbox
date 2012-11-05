c819c999e739595d5109402aa1ef3ae4b90c4fb0
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-11-05 15:53:26 +0000

    stuff

--- tdd/src/main/java/com/work/tdd/euler/hard/Problem98.java
+++ tdd/src/main/java/com/work/tdd/euler/hard/Problem98.java
@@ -1,13 +1,14 @@
 package com.work.tdd.euler.hard;
 
+import com.google.common.base.Predicate;
 import com.work.tdd.euler.medium.Utility;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import javax.annotation.Nullable;
+import java.util.*;
 import java.util.logging.Logger;
 
+import static com.google.common.collect.Collections2.filter;
 import static com.google.common.collect.Lists.charactersOf;
 import static com.google.common.collect.Lists.newArrayList;
 import static org.testng.Assert.assertFalse;
@@ -28,20 +29,40 @@ public class Problem98 {
         return Utility.readFile(resource).get(0).split(",");
     }
 
-    private static void explore() {
+    private static Collection<String> explore() {
+        int max = 0;
         String[] inputs = inputs("words.txt");
+        Collection<String> maxStrings = new HashSet<>();
         for (String s1 : inputs) {
             for (String s2 : inputs) {
                 if (!s1.equals(s2) && isAnagram(s1, s2)) {
-                    logger.info("Inputs " + s1 + " and " + s2);
+                    if (s1.length() >= max) {
+                        max = s1.length();
+                        maxStrings.add(s1);
+                        maxStrings.add(s2);
+                    }
                 }
             }
         }
+        maxStrings = filter(maxStrings, lengthPredicate(max));
+        logger.info("Maximum length " + maxStrings);
+        return maxStrings;
+    }
+
+    private static Predicate<String> lengthPredicate(final int length) {
+        return new Predicate<String>() {
+            @Override
+            public boolean apply(@Nullable String input) {
+                return length == input.toString().length();
+            }
+        };
     }
 
     @Test
     public void testBits() {
-        explore();
+        Collection<String> anagrams = explore();
+        assertTrue(anagrams.contains("\"INTRODUCE\""));
+        assertTrue(anagrams.contains("\"REDUCTION\""));
         assertTrue(isAnagram("CARE", "RACE"));
         assertFalse(isAnagram("CARER", "RACE"));
     }
