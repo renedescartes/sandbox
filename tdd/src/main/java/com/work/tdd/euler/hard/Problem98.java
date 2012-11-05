201c9876aaab17234d5f7295ac1659986c4ec868
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-11-05 16:29:18 +0000

    stuff

--- tdd/src/main/java/com/work/tdd/euler/hard/Problem98.java
+++ tdd/src/main/java/com/work/tdd/euler/hard/Problem98.java
@@ -1,6 +1,7 @@
 package com.work.tdd.euler.hard;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.work.tdd.euler.medium.Utility;
 import com.work.tdd.euler.util.Tuple;
@@ -21,6 +22,37 @@ import static org.testng.Assert.*;
 public class Problem98 {
     private static final Logger logger = Logger.getLogger(Problem98.class.getName());
 
+    public static long largestNumber(Integer[] arrangement) {
+        long start = largestWithNDigits(arrangement.length);
+        long end = smallestWithNDigits(arrangement.length);
+        for (long i = start; i >= end; i--) {
+            Long square = i * i;
+            if (distinctDigits(square)) {
+                logger.info("i = " + i + " square " + square);
+                Long convert = convert(square, arrangement);
+                if (isPerfectSquare(convert)) {
+                    return Math.max(square, convert);
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Test
+    public void testSimple() {
+        assertEquals(largestNumber(new Integer[]{3, 8, 5, 6, 7, 2, 0, 4, 1}), 24);
+    }
+
+    @Test
+    public void testBits() {
+        assertEquals(anagramRepresentation("INTRODUCE", "REDUCTION"), new Integer[]{3, 8, 5, 6, 7, 2, 0, 4, 1});
+        assertEquals(convert(712347432L, new Integer[]{3, 8, 5, 6, 7, 2, 0, 4, 1}).longValue(), 327432741);
+        Collection<Tuple<String, String>> anagrams = explore();
+        assertTrue(anagrams.contains(new Tuple<>("INTRODUCE", "REDUCTION")));
+        assertTrue(isAnagram("CARE", "RACE"));
+        assertFalse(isAnagram("CARER", "RACE"));
+    }
+
     private static boolean isAnagram(String s1, String s2) {
         List<Character> c1 = newArrayList(charactersOf(s1));
         Collections.sort(c1);
@@ -29,21 +61,13 @@ public class Problem98 {
         return Arrays.equals(c1.toArray(), c2.toArray());
     }
 
-    private static String[] inputs(String resource) {
-        return Utility.readFile(resource).get(0).split(",");
-    }
-
     private static Collection<Tuple<String, String>> explore() {
-        int max = 0;
-        String[] inputs = inputs("words.txt");
+        String[] inputs = Utility.readFile("words.txt").get(0).split(",");
         Collection<Tuple<String, String>> tuples = new ArrayList<>();
         for (String s1 : inputs) {
             for (String s2 : inputs) {
                 if (!s1.equals(s2) && isAnagram(s1, s2)) {
-                    if (s1.length() >= max) {
-                        max = s1.length();
-                        tuples.add(new Tuple<>(s1, s2));
-                    }
+                    tuples.add(new Tuple<>(s1.replaceAll("\"", ""), s2.replaceAll("\"", "")));
                 }
             }
         }
@@ -61,37 +85,15 @@ public class Problem98 {
         return Long.valueOf(StringUtils.join(newDigits, ""));
     }
 
-    public static long largestNumber(Integer[] arrangement) {
-        long start = largestWithNDigits(arrangement.length);
-        long end = smallestWithNDigits(arrangement.length);
-        for (long i = start; i >= end; i--) {
-            Long square = i * i;
-            if (distinctDigits(square)) {
-                logger.info("i = " + i + " square " + square);
-                Long convert = convert(square, arrangement);
-                if (isPerfectSquare(convert)) {
-                    return Math.max(square, convert);
-                }
-            }
-        }
-        return 0;
-    }
-
     private static boolean distinctDigits(Long n) {
         return n.toString().length() == Sets.newHashSet(digits(n)).size();
     }
 
-    @Test
-    public void testSimple() {
-        assertEquals(largestNumber(new Integer[]{3, 8, 5, 6, 7, 2, 0, 4, 1}), 24);
-    }
-
-    @Test
-    public void testBits() {
-        assertEquals(convert(712347432L, new Integer[]{3, 8, 5, 6, 7, 2, 0, 4, 1}).longValue(), 327432741);
-        Collection<Tuple<String, String>> anagrams = explore();
-        assertTrue(anagrams.contains(new Tuple<>("\"INTRODUCE\"", "\"REDUCTION\"")));
-        assertTrue(isAnagram("CARE", "RACE"));
-        assertFalse(isAnagram("CARER", "RACE"));
+    private static Integer[] anagramRepresentation(String s1, String s2) {
+        Collection<Integer> anagram = new ArrayList<>();
+        for (Character c : Lists.charactersOf(s2)) {
+            anagram.add(s1.indexOf(c));
+        }
+        return anagram.toArray(new Integer[anagram.size()]);
     }
 }
