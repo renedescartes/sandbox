47c2af70688cc0434e5602b14c3d602d27284c7f
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-11-05 16:19:03 +0000

    stuff

--- tdd/src/main/java/com/work/tdd/euler/hard/Problem98.java
+++ tdd/src/main/java/com/work/tdd/euler/hard/Problem98.java
@@ -1,17 +1,15 @@
 package com.work.tdd.euler.hard;
 
 import com.google.common.base.Preconditions;
-import com.google.common.base.Predicate;
 import com.google.common.collect.Sets;
 import com.work.tdd.euler.medium.Utility;
+import com.work.tdd.euler.util.Tuple;
 import org.apache.commons.lang.StringUtils;
 import org.testng.annotations.Test;
 
-import javax.annotation.Nullable;
 import java.util.*;
 import java.util.logging.Logger;
 
-import static com.google.common.collect.Collections2.filter;
 import static com.google.common.collect.Lists.charactersOf;
 import static com.google.common.collect.Lists.newArrayList;
 import static com.work.tdd.euler.medium.Utility.digits;
@@ -33,33 +31,22 @@ public class Problem98 {
         return Utility.readFile(resource).get(0).split(",");
     }
 
-    private static Collection<String> explore() {
+    private static Collection<Tuple<String, String>> explore() {
         int max = 0;
         String[] inputs = inputs("words.txt");
-        Collection<String> maxStrings = new HashSet<>();
+        Collection<Tuple<String, String>> tuples = new ArrayList<>();
         for (String s1 : inputs) {
             for (String s2 : inputs) {
                 if (!s1.equals(s2) && isAnagram(s1, s2)) {
                     if (s1.length() >= max) {
                         max = s1.length();
-                        maxStrings.add(s1);
-                        maxStrings.add(s2);
+                        tuples.add(new Tuple<>(s1, s2));
                     }
                 }
             }
         }
-        maxStrings = filter(maxStrings, lengthPredicate(max));
-        logger.info("Maximum length " + maxStrings);
-        return maxStrings;
-    }
-
-    private static Predicate<String> lengthPredicate(final int length) {
-        return new Predicate<String>() {
-            @Override
-            public boolean apply(@Nullable String input) {
-                return length == input.toString().length();
-            }
-        };
+        logger.info("Tuples " + tuples);
+        return tuples;
     }
 
     private static Long convert(Long number, Integer[] arrangement) {
@@ -100,9 +87,8 @@ public class Problem98 {
     @Test
     public void testBits() {
         assertEquals(convert(712347432L, new Integer[]{3, 8, 5, 6, 7, 2, 0, 4, 1}).longValue(), 327432741);
-        Collection<String> anagrams = explore();
-        assertTrue(anagrams.contains("\"INTRODUCE\""));
-        assertTrue(anagrams.contains("\"REDUCTION\""));
+        Collection<Tuple<String, String>> anagrams = explore();
+        assertTrue(anagrams.contains(new Tuple<>("\"INTRODUCE\"", "\"REDUCTION\"")));
         assertTrue(isAnagram("CARE", "RACE"));
         assertFalse(isAnagram("CARER", "RACE"));
     }
