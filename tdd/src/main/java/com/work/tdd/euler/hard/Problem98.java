ab08e490751d610a25c03dee5ac9db08f0a91f06
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-11-05 15:38:10 +0000

    stuff

--- tdd/src/main/java/com/work/tdd/euler/hard/Problem98.java
+++ tdd/src/main/java/com/work/tdd/euler/hard/Problem98.java
@@ -1,10 +1,12 @@
 package com.work.tdd.euler.hard;
 
+import com.work.tdd.euler.medium.Utility;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 import static com.google.common.collect.Lists.charactersOf;
 import static com.google.common.collect.Lists.newArrayList;
@@ -12,6 +14,7 @@ import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class Problem98 {
+    private static final Logger logger = Logger.getLogger(Problem98.class.getName());
 
     private static boolean isAnagram(String s1, String s2) {
         List<Character> c1 = newArrayList(charactersOf(s1));
@@ -21,8 +24,24 @@ public class Problem98 {
         return Arrays.equals(c1.toArray(), c2.toArray());
     }
 
+    private static String[] inputs(String resource) {
+        return Utility.readFile(resource).get(0).split(",");
+    }
+
+    private static void explore() {
+        String[] inputs = inputs("words.txt");
+        for (String s1 : inputs) {
+            for (String s2 : inputs) {
+                if (!s1.equals(s2) && isAnagram(s1, s2)) {
+                    logger.info("Inputs " + s1 + " and " + s2);
+                }
+            }
+        }
+    }
+
     @Test
     public void testBits() {
+        explore();
         assertTrue(isAnagram("CARE", "RACE"));
         assertFalse(isAnagram("CARER", "RACE"));
     }
